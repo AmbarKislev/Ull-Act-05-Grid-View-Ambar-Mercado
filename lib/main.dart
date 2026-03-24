57070f2d97e7dd4b15d50d4bc881d41a2c6ad9b0
a.23308051280283 <a.23308051280283@cbtis128.edu.mx>
2026-03-24 16:57:38 +0000

    act

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: depend_on_referenced_packages
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,117 +8,113 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const HomePageWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePageWidget extends StatefulWidget {
+  const HomePageWidget({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePageWidget> createState() => _HomePageWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageWidgetState extends State<HomePageWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget crearTarjeta(String nombre, String categoria, String url) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 60),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
+              child: Image.network(
+                url,
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+                // Si la imagen falla, muestra un icono de aviso
+                errorBuilder: (context, error, stackTrace) => Container(
+                  color: Colors.grey[200],
+                  child: const Icon(Icons.image_not_supported, color: Colors.red),
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: const AlignmentDirectional(0, 0.45),
+            child: Text(
+              nombre,
+              style: GoogleFonts.inter(fontWeight: FontWeight.w600, fontSize: 14),
+            ),
+          ),
+          Align(
+            alignment: const AlignmentDirectional(0, 0.65),
+            child: Text(
+              categoria,
+              style: GoogleFonts.inter(fontSize: 11, color: Colors.grey),
+            ),
+          ),
+          const Align(
+            alignment: AlignmentDirectional(0, 0.90),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.star_purple500_rounded, size: 18),
+                Icon(Icons.star_purple500_rounded, size: 18),
+                Icon(Icons.star_purple500_rounded, size: 18),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: const Color(0xFFF5F5F5),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.brown,
+        title: const Text('ENTRE MIGAS', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: GridView.count(
+        padding: const EdgeInsets.all(12),
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 0.8,
+        children: [
+          crearTarjeta('Concha', 'Panaderia Mexicana', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/refs/heads/main/concha-removebg-preview.png'),
+          crearTarjeta('Croissant', 'Panaderia Francesa', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/refs/heads/main/croissant.webp'),
+          crearTarjeta('Matcha', 'Bebidas', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/refs/heads/main/matcha.jfif'),
+          crearTarjeta('Chocolate', 'Bebidas', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/main/chocolate.jpg'),
+          crearTarjeta('Muffins', 'Reposteria', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/main/muffins.jpg'),
+          crearTarjeta('Moka', 'Bebidas', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/main/moka.webp'),
+          // Nota: Corregido a "espresso.webp" que es el nombre real en tu repo
+          crearTarjeta('Expresso', 'Bebidas', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/main/espresso.webp'),
+          crearTarjeta('Brioche', 'Reposteria', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/main/Brioche.jpg'),
+          crearTarjeta('Dona', 'Reposteria', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/main/dona.jpg'),
+          crearTarjeta('Bagel', 'Panaderia', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/refs/heads/main/artesanal.jpg'),
+          crearTarjeta('Cheesecake', 'Reposteria', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/refs/heads/main/muffins.jpg'),
+          crearTarjeta('Latte', 'Bebidas', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/main/latte.jpg'),
+          crearTarjeta('Capuchino', 'Bebidas', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/refs/heads/main/cafe%20frrio.png'),
+          crearTarjeta('Americano', 'Bebidas', 'https://raw.githubusercontent.com/AmbarKislev/pannnnnn/refs/heads/main/espresso.webp'),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
